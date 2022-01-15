19dc4a88ba0f45c88afd4dfb883c240954e227d5
SangTran <sang.tran@adurolife.com>
2022-01-15 14:37:18 +0700

    create models, run migration to create tables

--- nodejs/src/migrations/20220115061302-create-allcode.js
+++ nodejs/src/migrations/20220115061302-create-allcode.js
@@ -1,41 +1,23 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Allcodes', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
+      key: {
         type: Sequelize.STRING
       },
-      password: {
+      type: {
         type: Sequelize.STRING
       },
-      firstName: {
+      valueEn: {
         type: Sequelize.STRING
       },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      address: {
-        type: Sequelize.STRING
-      },
-      gender: {
-        type: Sequelize.BOOLEAN
-      },     
-      phoneNumber: {
-        type: Sequelize.STRING
-      },
-      roleId: {
-        type: Sequelize.STRING
-      },
-      image: {
-        type: Sequelize.STRING
-      },
-      positionId: {
+      valueVi: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -49,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Allcodes');
   }
 };
--- nodejs/src/migrations/20220115070636-create-clinic.js
+++ nodejs/src/migrations/20220115070636-create-clinic.js
@@ -1,41 +1,23 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Clinics', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
+      name: {
         type: Sequelize.STRING
       },
       address: {
         type: Sequelize.STRING
       },
-      gender: {
-        type: Sequelize.BOOLEAN
-      },     
-      phoneNumber: {
-        type: Sequelize.STRING
-      },
-      roleId: {
-        type: Sequelize.STRING
+      description: {
+        type: Sequelize.TEXT
       },
       image: {
-        type: Sequelize.STRING
-      },
-      positionId: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -49,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Clinics');
   }
 };
