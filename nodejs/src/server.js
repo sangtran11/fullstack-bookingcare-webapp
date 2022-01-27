ff28304aadc287f3eb4f5413a506782b2e354f27
SangTran <sang.tran@adurolife.com>
2022-01-27 18:49:33 +0700

    config cors, implement api login

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -3,9 +3,11 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import connectDb from "./config/connectDB";
+import cors from "cors";
 require("dotenv").config();
 
 let app = express();
+app.use(cors({ origin: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- nodejs/src/services/userServices.js
+++ nodejs/src/services/userServices.js
@@ -7,52 +7,54 @@ let handleUserLogin = (email, password) => {
       let data = {};
       let isExistEmail = await checkExistEmail(email);
       if (isExistEmail) {
-        let user = await db.User.findOne({ 
-          where: { email: email }, 
-          attributes: ['email', "roleId", "password"],
-          raw: true
+        let user = await db.User.findOne({
+          where: { email: email },
+          attributes: ["email", "roleId", "password"],
+          raw: true,
         });
         if (user) {
           let comparePw = bcrypt.compareSync(password, user.password);
           if (comparePw) {
             delete user.password;
-            data.errCode = 0;
-            data.msg = "Ok !"
+            data.msg = "Ok !";
             data.userInfo = user;
           } else {
             data.errCode = 1;
-            data.msg = "Wrong Password"
+            data.msg = "Wrong Password";
           }
         } else {
           data.errCode = 1;
-          data.msg = "User isn't found ..."
+          data.msg = "User isn't found ...";
         }
       } else {
         data.errCode = 1;
-        data.msg = "Email isn't exist, Please try other email ..."
+        data.msg = "Email isn't exist, Please try other email ...";
       }
       resolve(data);
     } catch (error) {
-      reject(error)
+      reject(error);
     }
-  })
-}
+  });
+};
 
 let checkExistEmail = (userEmail) => {
   return new Promise(async (resolve, reject) => {
     try {
-      let email = await db.User.findOne({ where: { email: userEmail }, raw: true });
+      let email = await db.User.findOne({
+        where: { email: userEmail },
+        raw: true,
+      });
       if (email) {
-        resolve(true)
+        resolve(true);
       } else {
-        resolve(false)
+        resolve(false);
       }
     } catch (error) {
       reject(error);
     }
-  })
-}
+  });
+};
 
 module.exports = {
-  handleUserLogin
-}
+  handleUserLogin,
+};
